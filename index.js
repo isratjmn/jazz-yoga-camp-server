857e1e3e3a12c8212c5e697c1202d90503ea3784
IsratMoon <mnisrat.j@gmail.com>
2023-06-11 05:08:18 +0600

    Role API added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // MIDDLEWARE
@@ -42,8 +42,7 @@ async function run() {
 			res.send(result);
 		});
 
-
-        app.get("/carts", async (req, res) => {
+		app.get("/carts", async (req, res) => {
 			const email = req.query.email;
 			if (!email) {
 				res.send([]);
@@ -58,12 +57,20 @@ async function run() {
 			const query = { email: email };
 			const result = await cartCollection.find(query).toArray();
 			res.send(result);
-		})
+		});
 
 		app.post("/carts", async (req, res) => {
 			const item = req.body;
-            console.log(item);
+			console.log(item);
 			const result = await cartCollection.insertOne(item);
+			res.send(result);
+		});
+
+		app.delete("/carts/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			console.log(query);
+			const result = await cartCollection.deleteOne(query);
 			res.send(result);
 		});
 
@@ -85,6 +92,32 @@ async function run() {
 				return res.send({ message: "User Already Exists" });
 			}
 			const result = await usersCollection.insertOne(user);
+			res.send(result);
+		});
+
+		app.patch("/users/admin/:id", async (req, res) => {
+			const id = req.params.id;
+			console.log(id);
+			const filter = { _id: new ObjectId(id) };
+			const updateDoc = {
+				$set: {
+					role: "admin",
+				},
+			};
+			const result = await usersCollection.updateOne(filter, updateDoc);
+			res.send(result);
+		});
+
+		app.patch("/users/instructor/:id", async (req, res) => {
+			const id = req.params.id;
+			console.log(id);
+			const filter = { _id: new ObjectId(id) };
+			const updateDoc = {
+				$set: {
+					role: "instructor",
+				},
+			};
+			const result = await usersCollection.updateOne(filter, updateDoc);
 			res.send(result);
 		});
 
